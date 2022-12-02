ff5928b4abf5d7992fdd5522bb4f9ef25418d01f
tkniki <trankimniki@gmail.com>
2022-12-02 16:35:23 +0100

    Returning max calories carried by an elf. Solved.

--- 2022/Day01_CalorieCounting/Day01_CalorieCounting/Program.cs
+++ 2022/Day01_CalorieCounting/Day01_CalorieCounting/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(MaxCalorie(CaloriesCarriedByElves("Input.txt")));
+
+static int MaxCalorie(List<int> list)
+{
+    return list.Max();
+}
+
+static List<int> CaloriesCarriedByElves(string inputfile)
+{
+    List<int> calorieList = new List<int>();
+    using (StreamReader streamReader = new StreamReader(inputfile))
+    {
+        string line;
+        int caloriesCarriedByElf = 0;
+        while ((line = streamReader.ReadLine()) != null)
+        {
+            if (String.IsNullOrEmpty(line))
+            {
+                calorieList.Add(caloriesCarriedByElf); // the amount of calories one elf carries
+                caloriesCarriedByElf = 0;
+            }
+            else
+            {
+                caloriesCarriedByElf += Convert.ToInt32(line);
+            }
+        }
+    }
+    return calorieList;
+}
